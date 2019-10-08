eac97ca1fb7445707fd728054b037771df02c2e8
Sebastian Napora <sebastian.napora@corp.smtsoftware.com>
2019-10-08 21:35:05 +0200

    Adding react.fc to find and check props

--- src/components/Todo/List.tsx
+++ src/components/Todo/List.tsx
@@ -1,28 +1,41 @@
 import * as React from 'react';
 import Todo from './Todo';
 
-interface TestProps<T> {
+// interface TestProps<T> {
+//     // children: (item: T) => React.ReactNode
+//     items: Array<T>
+//     item: [{title: string, id: string}] 
+//     completed: boolean
+//     onCompleted: Function
+//     onDeleted: Function
+//     // title: string
+//     // id: string | null
+// }
+
+
+interface TestProps {
     // children: (item: T) => React.ReactNode
-    items: Array<T>
-    item: [{title: string, id: string}] 
+    items: []
+    item: [{ title: string, id: string }]
     completed: boolean
     onCompleted: Function
     onDeleted: Function
     // title: string
-    // id: string | null
+    // id: number | null
 }
 
-function List<T>({items, completed, onCompleted, onDeleted}: TestProps<T>) {
-    let todos = items.map(item => {
+const List: React.FC<TestProps> = props => {
+    let todos = props.items.map((item, idx) => {
+        console.log(idx)
         return (
             <Todo
-                // key={title}
+                key={idx}
                 {...item}
-                completed={completed}
-                onCompleted={onCompleted}
-                onDeleted={onDeleted}
-                title={item.title}
-                id={item.id} 
+                completed={props.completed}
+                onCompleted={props.onCompleted}
+                onDeleted={props.onDeleted}
+                // title={ props.items ||props.item[0].title}
+                // id={props.item[0].id} 
             />
         );
     });
@@ -32,5 +45,26 @@ function List<T>({items, completed, onCompleted, onDeleted}: TestProps<T>) {
         </ul>
     );
 }
+// function List<T>({items, completed, onCompleted, onDeleted}: TestProps<T>) {
+//     let todos = items.map(item => {
+//         return (
+//             <Todo
+//                 // key={title}
+//                 {...item}
+//                 completed={completed}
+//                 onCompleted={onCompleted}
+//                 onDeleted={onDeleted}
+//                 title={item.title}
+//                 id={item.id} 
+//             />
+//         );
+//     });
+//     return (
+//         <ul className="todo-list">
+//             {todos}
+//         </ul>
+//     );
+// }
+
 
 export default List;
