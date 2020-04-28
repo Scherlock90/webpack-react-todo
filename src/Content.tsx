63b04d9aa1980ed64fb2fe164b2346f38621a94d
Sebastian Napora <sebastian.napora@intive.com>
2020-04-28 14:51:38 +0200

    change in package.json command deploy

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Nav } from "./components/nav/Nav";
 
 import "./style/app.css";
 import { routesArray } from "./static/Routes";
 
 export const Content = () => (
-  <BrowserRouter>
+  <BrowserRouter basename={"/webpack-react-todo"}>
     <Nav />
     <Switch>
       {routesArray.map(({ exact, path, component }, index) => (
--- src/components/home-page/HomePage.tsx
+++ src/components/home-page/HomePage.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function() {
   return (
     <div className={"container-home-page"}>
-      <h1 className={"main-title"}> WebPack custom configuration </h1>
+      <h1 className={"main-title"}> WebPack Custom Configuration </h1>
     </div>
   );
 }
